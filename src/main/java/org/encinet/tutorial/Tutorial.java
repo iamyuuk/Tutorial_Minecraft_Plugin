95d04532304087e09072e71c95731815c3c4a0f3
iamyuuk <2704804982@qq.com>
2024-08-20 17:43:21 +0800

    权限 & 判断命令执行者是否为玩家

--- src/main/java/org/encinet/tutorial/Tutorial.java
+++ src/main/java/org/encinet/tutorial/Tutorial.java
@@ -3,6 +3,7 @@ package org.encinet.tutorial;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
@@ -29,7 +30,12 @@ public final class Tutorial extends JavaPlugin {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        sender.sendMessage("你成功执行了tutorial指令.");
-        return true;
-    }
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (player.hasPermission("tutorial.main")) {
+                    player.sendMessage("因为你拥有tutorial.main权限，所以你收到了这条消息");
+                    return true;
+                } else { player.sendMessage("你没有权限执行此命令!"); return false;}
+            } else { sender.sendMessage("只有玩家才能执行此命令"); return false;}
+        }
 }
